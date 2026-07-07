7f072b7146ea881c44d8907147004836d01ed64f
latha123136 <lathakruthu@gmail.com>
2026-07-07 15:20:05 +0530

    Completed Exercise 11 - Dependency Injection

---
+++ week1/DesignPatternsAndPrinciples/Exercise11-DependencyInjection/CustomerService.java
@@ -0,0 +1,14 @@
+public class CustomerService {
+
+    private CustomerRepository repository;
+
+    // Constructor Injection
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    public void getCustomerDetails(int id) {
+        System.out.println(repository.findCustomerById(id));
+    }
+
+}
---
+++ week1/DesignPatternsAndPrinciples/Exercise11-DependencyInjection/README.md
@@ -0,0 +1,16 @@
+# Exercise 11 - Dependency Injection
+
+## Objective
+
+Implement Dependency Injection using Constructor Injection.
+
+## Pattern Used
+
+Dependency Injection
+
+## Files
+
+- CustomerRepository.java
+- CustomerRepositoryImpl.java
+- CustomerService.java
+- DependencyInjectionTest.java
